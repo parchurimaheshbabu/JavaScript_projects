64dc39e4b8a225d932f93535346da055c29040c4
Parchuri mahesh babu <136172133+parchurimaheshbabu@users.noreply.github.com>
2023-08-13 15:08:35 +0530

    Add files via upload

---
+++ calculator/cal.js
@@ -0,0 +1,15 @@
+function deter(k){
+    var i=document.getElementById('a1')
+    i.value+=k
+
+}
+function deter2(){
+    var i=document.getElementById('a1')
+    i.value=eval(i.value)
+
+}
+function deter3(){
+    var i=document.getElementById('a1')
+    i.value=''
+
+}
